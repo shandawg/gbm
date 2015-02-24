b14a02b2388670989e98910b0643b8399e72ef37
Paul Shannon <paulrobshannon@gmail.com>
2015-02-24 09:57:46 -0600

    testing to install from github branch

--- R/predict.gbm.R
+++ R/predict.gbm.R
@@ -108,16 +108,15 @@ predict.gbm <- function(object,newdata,n.trees,
       {
          predF <- exp(predF)
       } else
-      if(object$distribution$name=="asymmetric")
-      {
-          predF <- exp(predF)
-      } else
       if(object$distribution$name=="tweedie")
       {
          predF <- exp(predF)
       }
       else if (object$distribution$name == "adaboost"){
          predF <- 1 / (1 + exp(-2*predF))
+      }
+      else if (object$distribution$name=="asymmetric"){
+          predF <- exp(predF)
       }
       if(object$distribution$name=="multinomial")
       {
